21faad6bbda18ba52b48e9acaea77fc923a1fb8f
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-22 20:10:14 -0400

    putting in sql

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'play2learn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAMES': 'math_scores' 'word_scores',
+        'USER': 'postgres',
+        'PASSWORD': 'Pandora117!',
+        'HOST': 'localhost',
+        'PORT': 6623
     }
 }
 
