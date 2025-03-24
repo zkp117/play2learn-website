23d1ef59137a7858f38f5c51078c2b9ca1267703
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-24 14:47:26 -0400

    data .2

--- anagramhunt/models.py
+++ anagramhunt/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
 class Anagramhunt(models.Model):
     play = models.CharField(max_length=200)
 
     def __str__(self):
         return self.play
+class PlayerScores(models.Model):
+    wordgame = models.ForeignKey(Anagramhunt, on_delete=models.CASCADE)
+    score = models.IntegerField
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.wordgame.name} - {self.score}"
