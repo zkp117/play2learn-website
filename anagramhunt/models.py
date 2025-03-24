416b7b911de22ebe0a652ad46986eb4ee74c1e72
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-24 19:58:33 -0400

    upt. 3

--- anagramhunt/models.py
+++ anagramhunt/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 class Anagramhunt(models.Model):
     play = models.CharField(max_length=200)
 
     def __str__(self):
         return self.play
 class WordScore(models.Model):
-    wordgame = models.ForeignKey(User, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    wordgame = models.ForeignKey(Anagramhunt, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     score = models.IntegerField
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)  
--- mathfacts/models.py
+++ mathfacts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 class Mathfacts(models.Model):
     play = models.CharField(max_length=200)
 
@@ -7,7 +7,7 @@ class Mathfacts(models.Model):
         return self.play
 class MathScore(models.Model):
     mathgame = models.ForeignKey(Mathfacts, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     score = models.IntegerField
     created = models.DateTimeField(auto_now_add=True)  # Add this
     updated = models.DateTimeField(auto_now=True)  # Add this
