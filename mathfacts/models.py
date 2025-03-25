586d071853189a730388834d77fe46b7c2928ac7
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-25 15:19:42 -0400

    pt. 2

--- mathfacts/models.py
+++ mathfacts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from common.utils.text import unique_slug
 
 MATH_OPERATIONS = [
     ('1', 'Addition'),
@@ -21,8 +22,18 @@ class MathScore(models.Model):
     chosen_highest_number = models.CharField(max_length=100)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     score = models.IntegerField(default=0)
+    slug = models.SlugField(
+        max_length=50, unique=True, null=True, editable=False
+    )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.user.username} - Score: {self.score} - Level: {self.chosen_highest_number}"
--- mathfacts/urls.py
+++ mathfacts/urls.py
@@ -3,6 +3,6 @@ from .views import MathGameView, MathScoreView
 
 app_name = 'mathfacts'
 urlpatterns = [
-    path('mathfacts/<int:pk>/play/', MathGameView.as_view(), name='play'),
-    path('mathfacts/scores/', MathScoreView.as_view(), name='math_score_list')
+    path('mathfacts/<slug>/play/', MathGameView.as_view(), name='play'),
+    path('mathfacts//<slug>/scores/', MathScoreView.as_view(), name='math_score_list')
 ]
