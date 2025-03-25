8bcc3c5e22a4834ad64b2d94aca941204d765526
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-24 21:18:29 -0400

    upt. 8

--- anagramhunt/models.py
+++ anagramhunt/models.py
@@ -14,7 +14,7 @@ class Anagramhunt(models.Model):
         return self.play
 class WordScore(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    score = models.IntegerField
+    score = models.IntegerField(default = 0)
     wordlengths = models.CharField(
         max_length = 1,
         choices = WORD_LENGTHS,
@@ -22,6 +22,7 @@ class WordScore(models.Model):
     )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)  
-    
+
     def __str__(self):
-        return f"{self.user.username} - Word Score: {self.wordlengths}"
+        # Use get_wordlength_display() to return the human-readable choice value
+        return f"{self.user.username} - Word Score: {self.score} - Length: {self.get_wordlengths_display()}"
