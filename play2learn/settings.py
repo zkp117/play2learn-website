bbd73b27833bfdb90cd10d1eb544c4aafc934191
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-24 20:03:58 -0400

    upt. 4

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -12,13 +12,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # built-in django apps
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admindocs',
 
     # local apps
     'pages.apps.PagesConfig',
--- play2learn/urls.py
+++ play2learn/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('anagramhunt/', include('anagramhunt.urls')),
     path('mathfacts/', include('mathfacts.urls')),
     path('', include('pages.urls')),
