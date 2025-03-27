b97cc0e763d38ff95c048b7163f3fbfba2d67910
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-27 16:30:14 -0400

    pt. 8

--- play2learn/views.py
+++ play2learn/views.py
@@ -1,4 +1,13 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 class MyAccountView(TemplateView):
     template_name = "my_account.html" 
+
+def my_account(request):
+    # Your logic here, return the correct template
+    return render(request, 'myapp/my_account.html')
+
+def my_play2learn(request):
+    # Your logic here, return the correct template
+    return render(request, 'myapp/my_play2learn.html')
