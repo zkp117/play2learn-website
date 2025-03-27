b92970258ea6403febeef1df3ad3cc34cf8de9d2
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-27 16:46:29 -0400

    pt. 9

--- play2learn/views.py
+++ play2learn/views.py
@@ -4,10 +4,8 @@ from django.shortcuts import render
 class MyAccountView(TemplateView):
     template_name = "my_account.html" 
 
-def my_account(request):
-    # Your logic here, return the correct template
-    return render(request, 'myapp/my_account.html')
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
 def my_play2learn(request):
-    # Your logic here, return the correct template
     return render(request, 'myapp/my_play2learn.html')
--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from .views import CustomPasswordChangeView, MyAccountPageView
 
 urlpatterns = [
-    path(
-        "password/change/", CustomPasswordChangeView.as_view(),
-        name="account_change_password"
+    path("password/change/", CustomPasswordChangeView.as_view(),name="account_change_password"
     ),
     path('my-account/', MyAccountPageView.as_view(), name='my-account'),
 ]
