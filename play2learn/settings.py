bc1740e8a89272cc904ab89b7db15c2b512bd5d8
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-29 13:24:03 -0400

    score pt. 3

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # AUTHENTICATION SETTINGS
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_URL = 'account_login'
-LOGIN_REDIRECT_URL = 'pages:homepage'
+LOGIN_REDIRECT_URL = '/'
 
 ## django-allauth settings
 ACCOUNT_LOGIN_METHODS = {"email"}
