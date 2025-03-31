54061b3fcb1d92113f34f504d9c6fcc0b45620b0
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-31 18:09:49 -0400

    pt. 7

--- users/forms.py
+++ users/forms.py
@@ -27,3 +27,6 @@ class CustomUserChangeForm(forms.ModelForm):
             )
         }
 
+
+
+
--- users/views.py
+++ users/views.py
@@ -6,13 +6,19 @@ from allauth.account.views import PasswordChangeView
 from .forms import CustomUserChangeForm
 from django.contrib.messages.views import SuccessMessageMixin
 class CustomPasswordChangeView( SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
-    success_url = reverse_lazy('my_account')
+    success_url = reverse_lazy('my-account')
 class MyAccountPageView( SuccessMessageMixin, LoginRequiredMixin, UpdateView):
 
     model = get_user_model()
     form_class = CustomUserChangeForm
     success_message = 'Update Successful'
-    template_name = 'account/my_account.html'
+    success_url = reverse_lazy('my_account')
+    template_name = 'account/my-account.html'
 
     def get_object(self):
         return self.request.user
+    
+    def form_valid(self, form):
+            response = super().form_valid(form)
+            print("Form Errors:", form.errors)  # Debugging
+            return response
