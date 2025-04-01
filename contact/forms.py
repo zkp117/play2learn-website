a2c6a7b2a6fd144182211f5b3cb58bd3e702ab1e
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-01 18:30:44 -0400

    pt. 3

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class ContactApplicationForm(forms.Form):
+class ContactStaffForm(forms.Form):
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
--- contact/views.py
+++ contact/views.py
@@ -1,12 +1,12 @@
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 
-from .forms import ContactApplicationForm
+from .forms import ContactStaffForm
 
 class ContactAppView(FormView):
-    template_name = 'connect/contact_page.html'
-    form_class = ContactApplicationForm
-    success_url = reverse_lazy('contact:thanks')
+    template_name = 'connect/contact-page.html'
+    form_class = ContactStaffForm
+    success_url = reverse_lazy('jobs:thanks')
 
 class ContactAppThanksView(TemplateView):
     template_name = 'connect/thanks.html'
