7ea7047ce59fc09f29e0d895b2a060a11d3041ab
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-01 18:57:25 -0400

    send email conatct pt.1

--- contact/views.py
+++ contact/views.py
@@ -1,12 +1,33 @@
+import html
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 
+from common.utils.email_service import send_email
 from .forms import ContactStaffForm
 
 class ContactAppView(FormView):
     template_name = 'connect/contact_us.html'
     form_class = ContactStaffForm
     success_url = reverse_lazy('jobs:thanks')
+
+    def form_valid(self, form, instance):
+        first_name = instance.first_name(ContactStaffForm.first_name)
+        data = form.cleaned_data
+        to = 'neeneez2008@gmail.com'
+        subject = 'Feedback for Play2Learn'
+        content = f'''<p>Hey {instance.first_name}!</p>
+            <p>We have received your questions and feedback.
+            We will get back to you in 3 - 5 business days.</p>
+            <ol>'''
+        for key, value in data.items():
+            label = key.replace('_', ' ').title()
+            entry = html.escape(str(value), quote=False)
+            content += f'<li>{label}: {entry}</li>'
+        
+        content += '</ol>'
+
+        send_email(to, subject, content)
+        return super().form_valid(form)
 
 class ContactAppThanksView(TemplateView):
     template_name = 'connect/thanks.html'
