b8c22a6a6960e620364712039667209e44906f81
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-03 17:37:24 -0400

    fixed

--- users/models.py
+++ users/models.py
@@ -13,9 +13,14 @@ class CustomUser(AbstractUser):
         verbose_name="Date of Birth", null=True, blank=True
     )
     avatar = models.ImageField(upload_to='avatars/', blank=True,
-    help_text='Image must be 200px by 200px.',
+    help_text='Image must be 500px by 500px.',
     validators=[validate_avatar]
     )
+
+    def validate_avatar(value):
+        w, h = get_image_dimensions(value)
+        if w > 500 or h > 500:
+            raise ValidationError('Avatar must be no bigger than 500x500 pixels.')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} ({self.username})'
