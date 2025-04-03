5f7d5e8f589384c0351beec35851a1b7da4738fb
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-03 16:46:47 -0400

    putting images pt.1

--- contact/views.py
+++ contact/views.py
@@ -17,23 +17,26 @@ class ContactAppView(FormView):
         content = f'''<p>Hey {first_name}</p>
         <p>Questions & feedback</p>
         <ol>'''
-        
+
         for key, value in data.items():
             label = key.replace('_', ' ').title()
             entry = html.escape(str(value), quote=False)
             content += f'<li>{label}: {entry}</li>'
-            
+
         content += '</ol>'
-            
-            
+
+    # sends email to admin / sendgrid account owner (email I use is neeneez2008@gmail.com)
         send_email('neeneez2008@gmail.com', 'Questions & feedback', content)
 
-    
-        if 'email' in data and data['email']:
-            send_email(data['email'], 'Your Questions & feedback Received', 
-                   '<p>Thank you for reaching out to us! We will respond in 3 - 5 business days</p>')
+    # sending the email to the user (to the email address they entered in the form)
+        user_email = data.get('email')
+        if user_email:
+            send_email(
+                user_email,
+                'Your Questions & Feedback Received',
+                '<p>Thank you for reaching out to us! We will respond in 3 - 5 business days</p>'
+            )
 
         return super().form_valid(form)
-
 class ContactAppThanksView(TemplateView):
     template_name = 'connect/thanks.html'
--- play2learn/settings.py
+++ play2learn/settings.py
@@ -165,6 +165,10 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # BOTTOM OF settings.py
