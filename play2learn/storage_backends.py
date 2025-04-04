cdf79fc4f3b141688523de8ccfc630b44943cd34
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-04 12:39:42 -0400

    pt. 10

--- play2learn/storage_backends.py
+++ play2learn/storage_backends.py
@@ -1,18 +1,14 @@
-from storages.backends.s3 import S3Storage
-class StaticStorage(S3Storage):
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class StaticStorage(S3Boto3Storage):
     location = 'static'
     default_acl = 'public-read'
-    file_overwrite = True
 
-    def __init__(self, *args, **kwargs):
-        print("🚀 Using StaticStorage (S3) for static files!")
-        super().__init__(*args, **kwargs)
-class PublicMediaStorage(S3Storage):
+class PublicMediaStorage(S3Boto3Storage):
     location = 'media/public'
     default_acl = 'public-read'
-    file_overwrite = False
-class PrivateMediaStorage(S3Storage):
+
+class PrivateMediaStorage(S3Boto3Storage):
     location = 'media/private'
     default_acl = 'private'
-    file_overwrite = False
     custom_domain = False
