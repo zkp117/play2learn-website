2707919a970b216207ca09b4e0c335d060e9dede
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-04 12:53:30 -0400

    pt. 12

--- play2learn/storage_backends.py
+++ play2learn/storage_backends.py
@@ -1,14 +1,19 @@
-from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
+
 class StaticStorage(S3Boto3Storage):
+    """ Class for storing static files. """
     location = 'static'
     default_acl = 'public-read'
+    file_overwrite = True
 
 class PublicMediaStorage(S3Boto3Storage):
-    location = settings.PUBLIC_MEDIA_LOCATION
+    """ Class for storing public media files. """
+    location = 'media/public'
     default_acl = 'public-read'
+    file_overwrite = False
 
 class PrivateMediaStorage(S3Boto3Storage):
-    location = settings.PRIVATE_MEDIA_STORAGE
-    default_acl = 'private'
+    location = "media/private"
+    default_acl = "private"
+    file_overwrite = False
     custom_domain = False
