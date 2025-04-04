1bf4dd4d74553c6de120337a66a85d531031e12b
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-04 18:32:41 -0400

    pt. 15

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -175,7 +175,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_S3_REGION_NAME = 'us-east-2' 
 
 STATICFILES_STORAGE = 'play2learn.storage_backends.StaticStorage'
-DEFAULT_FILE_STORAGE = 'play2learn.storage_backends.PublicMediaStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 PRIVATE_FILE_STORAGE = 'play2learn.storage_backends.PrivateMediaStorage'
 
 STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/'
