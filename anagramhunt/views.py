5ce7246f88c5ed0fe2de0b646749b06fc1fd8475
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-06 11:23:04 -0400

    putting anagram in website pt. 1

--- anagramhunt/views.py
+++ anagramhunt/views.py
@@ -1,8 +1,6 @@
 from django.views.generic import TemplateView, ListView
 from django.shortcuts import render
 from .models import Anagramhunt 
-
-
 class AnagramGameView(TemplateView):
     def get(self, request, *args, **kwargs):
         wordgame = Anagramhunt.objects.get(pk=kwargs['pk'])
